727ee9b1e1e6bd00befe82be4ad72bcd6252c23b
B. Haki Martin <jojackblack@gmail.com>
2024-12-11 15:10:00 -0500

    created default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          {children}
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            A companion app to Draw Steel
+          </footer>
+        </div>
       </body>
     </html>
   );
